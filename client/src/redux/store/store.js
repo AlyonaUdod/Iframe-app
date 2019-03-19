babe2180921b386dacfcf962fd69048f1fac30d3
Your Name <freelspringisoul@gmail.com>
2019-03-19 23:02:44 +0200

    fix store

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducers from '../reducers/index.js';
 import thunk from 'redux-thunk'
 
-const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(rootReducers, compose(applyMiddleware(thunk), DevTools));
+// const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducers, compose(applyMiddleware(thunk)));
 
 export default store;
