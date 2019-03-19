2b7ae60f2ff154dabd6890ce370ab018cc21bcf2
Your Name <freelspringisoul@gmail.com>
2019-03-20 01:01:37 +0200

    fix clean

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import store from './redux/store/store';
 import App from './App.js';
 import * as serviceWorker from './serviceWorker';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 import './normalize.css';
 import './index.css';
 
